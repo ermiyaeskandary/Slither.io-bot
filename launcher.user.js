3372150b16d80515e03537609ca887565b1ccc5e
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-04-22 20:17:20 +0100

    Version Number

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io-bot-launcher
 // @namespace    http://slither.io/
-// @version      0.0.1
+// @version      0.0.3
 // @description  Slither.io bot launcher
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
