eac07e65511ffdab89173e3d401fc394ad1a8ada
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-04-24 17:59:49 +0100

    Combined the launcher code with the bot code
    launcher.user.js and bot.user.js combined. 
    Launcher no longer needed.

--- bot.user.js
+++ bot.user.js
@@ -1,12 +1,67 @@
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.0.3
+// @version      0.1.3
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
 // @grant        none
 // ==/UserScript==
+
+// Functions needed for the bot start here
+
+//UPDATE - removed the onemousemove listener
+window.onmousemove = function(e) {
+  /*if (e = e || window.event) {
+    if ("undefined" != typeof e.clientX) {
+      xm = e.clientX - ww / 2;
+      ym = e.clientY - hh / 2;
+    }
+  }*/
+};
+//Set fake mouse coordinates
+window.setMouseCoordinates = function (x, y) {
+    window.xm = x;
+    window.ym = y;
+};
+// return the coordinates relative to the center (snake position).
+window.mouseRelativeToCenter = function (x, y) {
+    mapX = x - getHeight() / 2;
+    mapY = y - getWidth() / 2;
+    return [mapX, mapY];
+};
+// Map to mouse coordinates
+window.mapToMouse = function (x, y) {
+    mouseX = (x - getX())*gsc;
+    mouseY = (y - getY())*gsc;
+    return [mouseX, mouseY];
+};
+// Canvas width
+window.getWidth = function () {
+    return window.ww;
+};
+// Canvas height
+window.getHeight = function () {
+    return window.hh;
+};
+// X coordinates on the screen
+window.getX = function () {
+    return window.px;
+};
+// Y coordinates on the screen
+window.getY = function () {
+    return window.py;
+};
+// Get scaling ratio
+window.getScale = function(){
+    return window.gsc;
+}
+
+window.launchBot = function(d){
+    return window.botInterval = setInterval(window.loop, d);
+}
+
+// Actual bot code starts from here
 
 //sorting function, from property 'distance'.
 window.sortFood = function (a, b) {
