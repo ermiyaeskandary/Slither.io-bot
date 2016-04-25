daa8f93c7ba2517fd18657a66e94d07e80aae95c
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-04-25 17:02:32 +0100

    Version Number

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.1.3
+// @version      0.1.4
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
