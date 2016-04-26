2f60e71ada28b524f7d8c03445407e66ae486227
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-04-26 16:12:10 +0100

    Commented - Formatting
    I love comments!

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.1.7
+// @version      0.1.8
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
@@ -9,86 +9,93 @@
 // ==/UserScript==
 // Functions needed for the bot
 // Set fake mouse coordinates
-window.setMouseCoordinates = function (x, y) {
+window.setMouseCoordinates = function(x, y) {
     window.xm = x;
     window.ym = y;
 };
 // Return the coordinates relative to the center (snake position).
-window.mouseRelativeToCenter = function (x, y) {
+window.mouseRelativeToCenter = function(x, y) {
     mapX = x - getHeight() / 2;
     mapY = y - getWidth() / 2;
     return [mapX, mapY];
 };
 // Map to mouse coordinates
-window.mapToMouse = function (x, y) {
+window.mapToMouse = function(x, y) {
     mouseX = (x - getX()) * gsc;
     mouseY = (y - getY()) * gsc;
     return [mouseX, mouseY];
 };
 // Canvas width
-window.getWidth = function () {
+window.getWidth = function() {
     return window.ww;
 };
 // Canvas height
-window.getHeight = function () {
+window.getHeight = function() {
     return window.hh;
 };
 // X coordinates on the screen
-window.getX = function () {
+window.getX = function() {
     return window.px;
 };
 // Y coordinates on the screen
-window.getY = function () {
+window.getY = function() {
     return window.py;
 };
 // Get scaling ratio
-window.getScale = function () {
-        return window.gsc;
+window.getScale = function() {
+    return window.gsc;
 };
 isBotRunning = false;
-    // Sets the interval for the bot loop
-window.launchBot = function (d) {
+// Starts the bot
+window.launchBot = function(d) {
     console.log("Starting Bot.");
     isBotRunning = true;
+    // Save the original slither.io onmousemove function so we can re enable it back later
     window.mousemovelistener = window.onmousemove;
+    // Removed the onemousemove listener so we can move the snake manually by setting coordinates
     window.onmousemove = undefined;
     return window.botInterval = setInterval(window.loop, d);
 };
-
-window.stopBot = function () {
+// Stops the bot
+window.stopBot = function() {
     console.log("Stopping Bot.");
+    // Re enable the original onmousemove function
     window.onmousemove = window.mousemovelistener;
     isBotRunning = false;
+    // Clear the interval which starts the bot
     return clearInterval(window.botInterval);
 };
 
-window.connectBot = function () {
+window.connectBot = function() {
     console.log("Connecting");
     window.stopBot();
     window.connect();
     setTimeout(window.launchBot(), 1000);
 };
-
+// Save the original slither.io onkeydown function so we can add stuff to it
 document.oldKeyDown = document.onkeydown;
-document.onkeydown = function (e) {
+// Re write the function with our function
+document.onkeydown = function(e) {
+    // Original slither.io onkeydown function + whatever is under it
     document.oldKeyDown(e);
-    if(e.keyCode === 84) isBotRunning ? window.stopBot() : window.launchBot(5);
+    // If the letter "t" is pressed, check if the bot is running. If it is, stop the bot. If it isn't, start the bot.
+    if (e.keyCode === 84) isBotRunning ? window.stopBot() : window.launchBot(5);
 };
 
 // Sorting function, from property 'distance'
-window.sortFood = function (a, b) {
+window.sortFood = function(a, b) {
     return a.distance - b.distance;
 };
 
 // Given an object (of which properties xx and yy are not null), return the object with an additional property 'distance'
-window.getDistanceFromMe = function (point) {
+window.getDistanceFromMe = function(point) {
     if (point === null) return null;
     point.distance = getDistance(px, py, point.xx, point.yy);
     return point;
 };
 
 // Get a distance from point (x1; y1) to point (x2; y2).
-window.getDistance = function (x1, y1, x2, y2) {
+window.getDistance = function(x1, y1, x2, y2) {
     // Calculate the vector coordinates.
     var xDistance = (x1 - x2);
     var yDistance = (y1 - y2);
@@ -100,8 +107,9 @@ window.getDistance = function (x1, y1, x2, y2) {
     return distance;
 };
 // Sort food based on distance
-window.getSortedFood = function () {
-    return window.foods.filter(function (val) {
+window.getSortedFood = function() {
+    // Filters the nearest food by getting the distance
+    return window.foods.filter(function(val) {
         return val !== null;
     }).map(getDistanceFromMe).sort(sortFood);
 };
@@ -123,7 +131,7 @@ window.loop = function () {
 };
 */
 // Removed the onemousemove listener so we can move the snake manually by setting coordinates
-window.onmousemove = function (e) {
+window.onmousemove = function(e) {
     /*if (e = e || window.event) {
       if ("undefined" != typeof e.clientX) {
         xm = e.clientX - ww / 2;
@@ -134,15 +142,15 @@ window.onmousemove = function (e) {
 // Actual bot code
 
 // Loop for running the bot
-window.loop = function () {
-        // If the game is running
-        if (playing) {
-            // Sort the food based on distance
-            var sortedFood = getSortedFood();
-            // Convert coordinates of the closest food using mapToMouse
-            var coordinatesOfClosestFood = window.mapToMouse(sortedFood[0].xx, sortedFood[0].yy);
-            // Set the mouse coordinates to the coordinates of the closest food
-            window.setMouseCoordinates(coordinatesOfClosestFood[0], coordinatesOfClosestFood[1]);
+window.loop = function() {
+    // If the game is running
+    if (playing) {
+        // Sort the food based on distance
+        var sortedFood = getSortedFood();
+        // Convert coordinates of the closest food using mapToMouse
+        var coordinatesOfClosestFood = window.mapToMouse(sortedFood[0].xx, sortedFood[0].yy);
+        // Set the mouse coordinates to the coordinates of the closest food
+        window.setMouseCoordinates(coordinatesOfClosestFood[0], coordinatesOfClosestFood[1]);
 
-        }
-    };
+    }
+};
