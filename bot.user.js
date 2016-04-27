ff4693a80d11f12a9bc25dbe2acef99f21375506
K00sKlust <k00sklust@outlook.com>
2016-04-27 20:14:14 +0200

    Increased Versionnumber

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.3.0
+// @version      0.3.1
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
