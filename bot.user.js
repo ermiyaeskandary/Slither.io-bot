88f0a74e2cf637c6880638e1cc2e49538254958b
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-07 09:24:24 +0100

    Decreased Cluster size
    Tested...

--- bot.user.js
+++ bot.user.js
@@ -18,7 +18,7 @@
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.7.1
+// @version      0.7.2
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
@@ -584,7 +584,7 @@ window.computeFoodGoal = function() {
         window.currentFoodY = window.currentFood.yy;
 
         // if see a large cluster then use acceleration
-        if (bestClusterSize > 60) {
+        if (bestClusterSize > 50) {
             window.foodAcceleration = 1;
         } else {
             window.foodAcceleration = 0;
