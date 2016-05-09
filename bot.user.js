38e7487c166548b7bb8a71268561141c426c2deb
chancity <hawbakerchancey@gmail.com>
2016-05-09 11:29:54 -0700

    Optimisation - Food clustering

--- bot.user.js
+++ bot.user.js
@@ -281,7 +281,6 @@ var bot = (function() {
     return {
         ranOnce: false,
         tickCounter: 0,
-        foodIndx: 0,
         isBotRunning: false,
         isBotEnabled: true,
         collisionPoint: {
@@ -360,14 +359,14 @@ var bot = (function() {
                 window.setSkin(window.snake, skin);
             }
         },
-
-        rotateSkin: function() {
-            if (!window.rotateskin) {
-                return;
-            }
-            bot.changeSkin();
-            setTimeout(bot.rotateSkin, 500);
-        },
+		
+		rotateSkin: function() {
+             if (!window.rotateskin) {
+                 return;
+             }
+             bot.changeSkin();
+             setTimeout(bot.rotateSkin, 500);
+         },
 
         // Adjust goal direction
         changeGoalCoords: function(circle1) {
@@ -379,10 +378,10 @@ var bot = (function() {
             }
         },
 
-        // Sorting function for food, from property 'distance'
-        sortFood: function(a, b) {
-            return a.distance - b.distance;
-        },
+        // Sorting function for food, from property 'clusterCount'
+		sortFood: function(a, b) {
+			return (a.clusterScore == b.clusterScore ? 0 : a.clusterScore / a.distance  >  b.clusterScore / b.distance  ? -1 : 1);
+		},
 
         // Sorting function for prey, from property 'distance'
         sortPrey: function(a, b) {
@@ -430,7 +429,7 @@ var bot = (function() {
             return avoid;
         },
 
-        // Sort food based on distance
+         // Sort food based on distance
         getSortedFood: function() {
             // Filters the nearest food by getting the distance
             return window.foods.filter(function(val) {
@@ -439,8 +438,46 @@ var bot = (function() {
                 var isInsideDangerAngles = canvas.isInsideAngle(val, window.snake.ang - 3 * Math.PI / 4, window.snake.ang - Math.PI / 4);
                 isInsideDangerAngles = isInsideDangerAngles || canvas.isInsideAngle(val, window.snake.ang + Math.PI / 4, window.snake.ang + 3 * Math.PI / 4);
                 return !(isInsideDangerAngles && (val.distance <= 150));
-            }).sort(bot.sortFood);
+            }).map(bot.foodClusters).sort(bot.sortFood);
         },
+		
+		foodClusters: function(food){
+			if (!food.clustered){
+				food.clusterScore = 0;
+				food.clusterxx = food.xx;
+				food.clusteryy = food.yy;
+				
+				var clusterSumX = 0;
+				var clusterSumY = 0;
+				var count = 0;
+				
+				for (var index in window.foods){
+					nearFood = window.foods[index];
+					if (nearFood !== null && nearFood.id !== food.id){
+						foodDistance = canvas.getDistance(food.xx,food.yy, nearFood.xx, nearFood.yy);
+						
+						if(foodDistance <= window.getSnakeWidth()*5){
+							count++;
+							food.clusterScore += nearFood.sz;
+							clusterSumX += nearFood.xx * nearFood.sz;
+							clusterSumY += nearFood.yy * nearFood.sz;
+							nearFood.clusterxx = nearFood.xx;
+							nearFood.clusteryy = nearFood.yy;
+							nearFood.clusterScore = nearFood.sz;
+							nearFood.clustered = true;
+						}
+					}
+				}
+				
+				if(count > 0){
+					food.clusterxx = clusterSumX / food.clusterScore;
+					food.clusteryy = clusterSumY / food.clusterScore;
+				}
+			}
+		
+		food.clustered = true;
+		return food;
+		},
 
         // Sort prey based on distance
         getSortedPrey: function() {
@@ -448,57 +485,6 @@ var bot = (function() {
             return window.preys.filter(function(val) {
                 return val !== null;
             }).map(canvas.getDistanceFromSnake).sort(bot.sortPrey);
-        },
-
-        computeFoodGoal: function() {
-            var sortedFood = bot.getSortedFood();
-
-            var bestClusterIndx = 0;
-            var bestClusterScore = 0;
-            var bestClusterAbsScore = 0;
-            var bestClusterX = 0;
-            var bestClusterY = 0;
-
-            // there is no need to view more points (for performance)
-            var nIter = Math.min(sortedFood.length, 300);
-            for (var i = 0; i < nIter; i += 2) {
-                var clusterScore = 0;
-                var clusterSize = 0;
-                var clusterAbsScore = 0;
-                var clusterSumX = 0;
-                var clusterSumY = 0;
-
-                var p1 = sortedFood[i];
-                for (var j = 0; j < nIter; ++j) {
-                    var p2 = sortedFood[j];
-                    var dist = canvas.getDistance(p1.xx, p1.yy, p2.xx, p2.yy);
-                    if (dist < 100) {
-                        clusterScore += p2.sz;
-                        clusterSumX += p2.xx * p2.sz;
-                        clusterSumY += p2.yy * p2.sz;
-                        clusterSize += 1;
-                    }
-                }
-                clusterAbsScore = clusterScore;
-                clusterScore /= Math.pow(p1.distance, 1.5);
-                if (clusterSize > 2 && clusterScore > bestClusterScore) {
-                    bestClusterScore = clusterScore;
-                    bestClusterAbsScore = clusterAbsScore;
-                    bestClusterX = clusterSumX / clusterAbsScore;
-                    bestClusterY = clusterSumY / clusterAbsScore;
-                    bestClusterIndx = i;
-                }
-            }
-
-            window.currentFoodX = bestClusterX;
-            window.currentFoodY = bestClusterY;
-
-            // if see a large cluster then use acceleration
-            if (bestClusterAbsScore > 50) {
-                window.foodAcceleration = 1;
-            } else {
-                window.foodAcceleration = 0;
-            }
         },
 
         // Defense mode - bot turns around in a circle
@@ -515,18 +501,24 @@ var bot = (function() {
 
                 // Save CPU by only calculating every Nth frame
                 bot.tickCounter++;
-                if (bot.tickCounter > 25) {
+                if (bot.tickCounter > 15) {
                     bot.tickCounter = 0;
 
-                    // Current food
-                    bot.computeFoodGoal();
-
-                    var coordinatesOfClosestFood = canvas.mapToMouse(window.currentFoodX, window.currentFoodY);
-                    window.goalCoordinates = coordinatesOfClosestFood;
+                    window.sortedFood = bot.getSortedFood();
+					window.currentFood = window.sortedFood[0]; 
+					
+                    var coordinatesOfClosestFood = canvas.mapToMouse(window.currentFood.clusterxx, window.currentFood.clusteryy);
+					window.goalCoordinates = coordinatesOfClosestFood;
                     // Sprint
-                    window.setAcceleration(window.foodAcceleration);
-                    // Check for preys, enough "length"
-                    if (window.preys.length > 0 && window.huntPrey) {
+					//use speed to go to larger clusters
+					if (window.currentFood.clusterScore >= 70){
+						if(window.currentFood.distance <= Math.pow(window.getSnakeLength(), 2) / 2){
+							setAcceleration(1);
+						}
+					} 
+					
+                    // Check for preys, enough "length", dont go after prey if current cluster is large
+                    if (window.preys.length > 0 && window.huntPrey && window.currentFood.clusterScore < 70) {
                         // Sort preys based on their distance relative to player's snake
                         window.sortedPrey = bot.getSortedPrey();
                         // Current prey
@@ -653,13 +645,13 @@ var userInterface = (function() {
                     console.log('Automatic Respawning set to: ' + window.autoRespawn);
                     userInterface.savePreference('autoRespawn', window.autoRespawn);
                 }
-                // Letter 'W' to auto rotate skin
+				// Letter 'W' to auto rotate skin
                 if (e.keyCode == 87) {
                     window.rotateskin = !window.rotateskin;
                     console.log('Auto skin rotator set to: ' + window.rotateskin);
                     userInterface.savePreference('rotateskin', window.rotateskin);
                     bot.rotateSkin();
-                }
+                 }
                 // Letter 'O' to change rendermode (visual)
                 if (e.keyCode === 79) {
                     window.toggleMobileRendering(!window.mobileRender);
@@ -859,7 +851,7 @@ window.loop = function() {
     userInterface.loadPreference('collisionDetection', true);
     userInterface.loadPreference('collisionRadiusMultiplier', 8);
     userInterface.loadPreference('defence', false);
-    userInterface.loadPreference('rotateskin', false);
+	userInterface.loadPreference('rotateskin', false);
     window.nick.value = userInterface.loadPreference('savedNick', 'Slither.io-bot');
 
     // Overlays
