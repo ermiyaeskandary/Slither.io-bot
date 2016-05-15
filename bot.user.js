1d3bf1a8d6892fc53fddcee4795be84f01331af4
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-15 14:39:40 +0100

    Version Number - TEST

--- bot.user.js
+++ bot.user.js
@@ -8,7 +8,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.9.1
+// @version      0.9.2
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
