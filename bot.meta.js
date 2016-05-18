6a7ed0826b60594298fa8097f39881df9d9c55c8
OneEyed <joelruiz2@gmail.com>
2016-05-17 19:06:11 -0500

    Fixed the UserScript

--- bot.meta.js
+++ bot.meta.js
@@ -5,10 +5,10 @@
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		 https://github.com/OneEyed/BehaviorTree.js/raw/master/btree-complete.min.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/master/bot.astar.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/master/bot.gridsystem.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/master/bot.behaviors.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/master/bot.user.js
+// @require		 https://github.com/OneEyed/BehaviorTree.js/raw/Grid-System/btree-complete.min.js
+// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.astar.js
+// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.gridsystem.js
+// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.behaviors.js
+// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.user.js
 // @grant        none
 // ==/UserScript==
