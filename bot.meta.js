004f2d5e53ce2e97c48d9486f824afc79d397b1a
OneEyed <joelruiz2@gmail.com>
2016-05-17 19:06:47 -0500

    Fixed BehaviorTree @require in meta file

--- bot.meta.js
+++ bot.meta.js
@@ -5,7 +5,7 @@
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		 https://github.com/OneEyed/BehaviorTree.js/raw/Grid-System/btree-complete.min.js
+// @require		 https://github.com/OneEyed/BehaviorTree.js/raw/master/btree-complete.min.js
 // @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.astar.js
 // @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.gridsystem.js
 // @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.behaviors.js
