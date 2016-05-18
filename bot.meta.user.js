b9d73cfebcc4a3a7b95924af2b1823f507cf1d8d
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-18 06:55:54 +0100

    Update bot.meta.user.js

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -5,11 +5,11 @@
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/btree-complete.min.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.astar.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.gridsystem.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.behaviors.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.user.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/btree-complete.min.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.astar.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.gridsystem.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.behaviors.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.user.js
 // @updateURL    https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.meta.js
 // @downloadURL  https://github.com/ErmiyaEskandary/Slither.io-bot/raw/Grid-System-r1/bot.meta.js
 // @grant        none
