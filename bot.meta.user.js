ee5d8d03b51e10e977a36450186f1fff08357579
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-20 13:28:38 +0100

    Fixed @require links - Should fix bot not working
    Left over from branch deletion

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -5,10 +5,10 @@
 // @description  Slither.io bot A*
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/btree-complete.min.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.astar.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.gridsystem.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.behaviors.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.user.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/blob/master/btree-complete.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/blob/master/bot.astar.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/blob/master/bot.gridsystem.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/blob/master/bot.behaviors.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/blob/master/bot.user.js
 // @grant        none
 // ==/UserScript==
