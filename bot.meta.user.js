03ed376be80b3df6dba339a8c21d4049c032411a
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-20 07:13:26 +0100

    Updated @require links

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -5,10 +5,10 @@
 // @description  Slither.io bot A*
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/btree-complete.min.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.astar.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.gridsystem.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.behaviors.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.user.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/btree-complete.min.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.astar.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.gridsystem.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.behaviors.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.user.js
 // @grant        none
 // ==/UserScript==
