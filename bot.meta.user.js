3c3fa5826d1f2e34a21f8c8e6de1e3eb03a25e76
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-20 07:21:46 +0100

    Grid system improvements
    Grid/A*/Behaviors all working as expected!! 
    - Bot is more cautious with sprinting
    - Bot tries to find path out of a surround (and sprints!)
    - Bot stops sprinting if to close to snake
    - Bot sprints towards high quality food
    - Bot stops sprinting if target is on its side to avoid infinite loops

---
+++ bot.meta.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Slither.io-bot A*
+// @namespace    http://slither.io/
+// @version      0.9.6
+// @description  Slither.io bot A*
+// @author       Ermiya Eskandary & Théophile Cailliau
+// @match        http://slither.io/
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/btree-complete.min.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.astar.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.gridsystem.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.behaviors.js
+// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/grid-system/bot.user.js
+// @grant        none
+// ==/UserScript==
