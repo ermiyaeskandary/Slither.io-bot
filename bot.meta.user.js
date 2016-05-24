80584c9664b3ba0676f6fd4442d40bba666ac074
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-24 17:14:50 +0100

    Update bot.meta.user.js

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -1,13 +1,14 @@
 ﻿// ==UserScript==
-// @name         Slither.io-bot A*// @namespace    http://slither.io/
+// @name         Slither.io-bot A*
+// @namespace    http://slither.io/
 // @version      1.0.1
 //@description  Slither.io bot A*
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/btree-complete.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.astar.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.gridsystem.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.behaviors.js
-// @require		 https://github.com/OneEyed/Slither.io-bot/raw/Grid-System/bot.user.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/btree-complete.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.astar.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.gridsystem.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.behaviors.js
+// @require		 https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.user.js
 // @grant        none
 // ==/UserScript==
