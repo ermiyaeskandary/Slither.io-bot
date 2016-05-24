a0f73b5b1a5b02402fe44c2d8d987d02c8342b8c
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-05-24 06:53:52 +0100

    Forgot...
    Just in case

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2016 Ermiya Eskandary & Théophile Cailliau and other contributors
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 // ==UserScript==
 // @name         Slither.io-bot A*
 // @namespace    http://slither.io/
