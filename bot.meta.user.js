975ddb21a1aa433f76bbb451f5e07990f011295b
Ermiya Eskandary <ErmiyaEskandary@users.noreply.github.com>
2016-06-05 10:54:10 +0100

    Update require links

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -11,10 +11,10 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // @description     Slither.io bot A*
 // @author          Ermiya Eskandary & Théophile Cailliau
 // @match           http://slither.io/
-// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/btree-complete.js
-// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.behaviors.js
-// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.astar.js
-// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.gridsystem.js
-// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.user.js
+// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/A---OLD/btree-complete.js
+// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/A---OLD/bot.behaviors.js
+// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/A---OLD/bot.astar.js
+// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/A---OLD/bot.gridsystem.js
+// @require         https://github.com/ErmiyaEskandary/Slither.io-bot/raw/A---OLD/bot.user.js
 // @grant           none
 // ==/UserScript==
